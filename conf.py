f1592cb7c647c63805b142cb34847cf53f86ac30
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-06-15 17:48:31 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "apihash"
-api_id = 1736372819
+api_hash = "78d1c032f3aa546ff5176d9ff0e7f341"
+api_id = 7386053
 bot_token = "5573297092:AAGL3m3FuKPCUvEvNZb4WGT6y9LLeqypcuY"
 authorized_users = []
 admin_user = 'topwin5'
