347a80bdf7e6652741f18e4f46b34e3e09db553f
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-08-12 07:18:28 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "78d1c032f3aa546ff5176d9ff0e7f341"
 api_id = 7386053
-bot_token = "5573297092:AAGL3m3FuKPCUvEvNZb4WGT6y9LLeqypcuY"
+bot_token = "5430093980:AAGJLNumlc3pvCiBdnGT-8rqAKdT5aIAnsQ"
 authorized_users = []
 admin_user = 'topwin5'
